44277091abd989948d97bbfcfdce914f1b786926
Georges Monette <georges@yorku.ca>
2017-07-11 17:27:54 -0400

    add misscode to show NA in 3d

---
+++ R/misscode.R
@@ -0,0 +1,34 @@
+#' Recode missing data
+#'
+#' Recodes missing data to a value less than the minimum value -- or to a non-missing
+#' code to indicate missingness for a factor -- to facilitate plotting in 3d
+#'
+#' @param x vector with possible missing values
+#' @param offset (default .1) how far below the minimum value as a proportion of the range
+#'
+#' @export
+misscode <- function(x,...) UseMethod('misscode')
+misscode.default <- function(x,...,offset = .1) {
+  rr <- range(x, na.rm = TRUE)
+  vmiss <- min(x,na.rm = TRUE) - offset * diff(rr)
+  nas <- is.na(x)
+  x[nas] <- vmiss
+  attr(x,'nas') <- nas
+  x
+}
+misscode.factor <- function(x, ...) {
+  nas <- is.na(x)
+  x <- addNA(x, ifany = TRUE)
+  attr(x,'nas') <- nas
+  x
+}
+misscode.data.frame <- function(x,...) {
+  x[] <- lapply(x[],misscode,...)
+  isna <- lapply( x, function(x) attr(x,'nas'))
+  #   disp(isna)
+  isna <- do.call(cbind,isna)
+  isna <- apply(isna, 1, sum)
+  #   disp(isna)
+  x$.nmiss <- isna
+  x
+}
